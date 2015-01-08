70f975e6a4f6d0f1493cbf30c2e388b6bf50e166
Tobias Dammers <tdammers@gmail.com>
2015-01-08 13:45:25 +0100

    Show featuretest errors

--- featuretest.sh
+++ featuretest.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-OUT=$(./.cabal-sandbox/bin/ginger templates/features.html); EXPECT=$(echo "$OUT" | grep '^expect:'); ACTUAL=$(echo "$OUT" | grep '^actual:'); diff -i -u <(echo "$EXPECT" | sed 's/^expect://') <(echo "$ACTUAL" | sed 's/^actual://')
+OUT=$(./.cabal-sandbox/bin/ginger templates/features.html)
+echo "$OUT" | grep -A3 '^templates/'
+EXPECT=$(echo "$OUT" | grep '^expect:')
+ACTUAL=$(echo "$OUT" | grep '^actual:')
+diff -i -u <(echo "$EXPECT" | sed 's/^expect://') <(echo "$ACTUAL" | sed 's/^actual://')
