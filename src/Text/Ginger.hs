1839e564acde244ccfcc51faf270705a3c61afa6
Tobias Dammers <tdammers@gmail.com>
2015-01-09 12:00:48 +0100

    Haddock for expression syntax implemented so far.

--- src/Text/Ginger.hs
+++ src/Text/Ginger.hs
@@ -34,7 +34,7 @@ module Text.Ginger
 -- There are two kinds of delimiters. `{% ... %}` and `{{ ... }}`. The first
 -- one is used to execute statements such as for-loops or assign values, the
 -- latter prints the result of an expression to the template.
---
+
 -- /Not implemented yet/: Jinja2 allows the programmer to override the default
 -- tags from @{% %}@ and @{{ }}@ to different tokens, e.g. @\<% %\>@ and @\<\<
 -- \>\>@.  Ginger does not currently support this.
@@ -44,23 +44,138 @@ module Text.Ginger
 -- passed in by the application. Variables may have attributes or elements
 -- on them you can access too. What attributes a variable has depends
 -- heavily on the application providing that variable.
---
--- You can use a dot (@.@) to access attributes of a variable, but
+
+-- | You can use a dot (@.@) to access attributes of a variable, but
 -- alternatively the so-called “subscript” syntax (@[]@) can be used. The
 -- following lines do the same thing:
---
--- > {{ foo.bar }}
--- > {{ foo['bar'] }}
---
--- It’s important to know that the curly braces are /not/ part of the
+
+-- | @
+-- {{ foo.bar }}
+-- {{ foo['bar'] }}
+-- @
+
+-- | It’s important to know that the curly braces are /not/ part of the
 -- variable, but the print statement. If you access variables inside tags
 -- don’t put the braces around them.
---
--- If a variable or attribute does not exist you will get back an undefined
+
+-- | If a variable or attribute does not exist you will get back an undefined
 -- value. What you can do with that kind of value depends on the
 -- application configuration: the default behavior is that it evaluates to
 -- an empty string if printed and that you can iterate over it, but every
 -- other operation fails.
+
+-- ** Expressions
+-- | Variables aren't the only thing that can go inside @{{ ... }}@; any valid
+-- Ginger expression can be used, and expressions can be constructed in many
+-- different ways. Note that all expressions are case sensitive: @null@ and
+-- @Null@ are not the same thing.
+-- Currently, the following constructs are available:
+
+-- *** Simple (\"Atomic\") Expressions
+-- **** Variable reference
+-- | Look up a variable in the current scope and return its value.
+
+-- | > {{ username }}
+
+-- **** String literals
+-- | A constant string. Strings can be single- or double-quoted.
+
+-- | > {{ "Hello, world!" }}
+
+-- **** Numeric literals
+-- | Numeric literals can be given in integer or decimal format:
+
+-- | @
+-- {{ 21 }}
+-- {{ 44.5 }}
+-- @
+
+-- **** Boolean literals
+-- | There are two boolean values, @true@ and @false@. You will not normally
+-- need to use them, as they are produced by boolean expressions such as
+-- comparisons, but they can be useful occasionally.
+
+-- | @
+-- {{ true }}
+-- {{ false }}
+-- @
+
+-- **** The Null literal
+-- | Represents the special /null/ value, which is used to signal the absence
+-- of a result or value. Looking up non-existent scope variables, for example,
+-- will produce Null.
+
+-- | > {{ null }}
+
+-- *** Data Structures
+-- **** List Literals
+-- | A list literal consists of a comma-separated list of expressions between
+-- square brackets:
+
+-- | > {{ [ foo, "bar", 1234 ] }}
+
+-- | Lists can contain any kind of expression, including other lists, so you can
+-- nest them:
+
+-- | > {{ [ foo, [ 1, 2, 3 ], "baz" ] }}
+
+-- **** Object Literals
+-- | An object is an unsorted key/value container, declared like this:
+
+-- | > {{ { "foo": "bar", "baz": "quux" } }}
+
+-- | Objects, like lists, can contain any kind of value. The keys, however, are
+-- restricted to strings; you can define them using any expression you like,
+-- but they are always converted to strings when inserting items into the
+-- container. This means that the following is valid:
+
+-- | > {{ { ["foo", 1]: "bar" } }}
+
+-- | It will, however, produce the same object as the following:
+
+-- | > {{ { "foo1": "bar" } }}
+
+-- | That's because converting @[\"foo\", 1]@ to a string produces @\"foo1\"@, and
+-- that is used as the key.
+
+-- *** Parentheses
+-- | Parentheses can be used to group expression constructs to override default
+-- precedence. Excess parentheses are simply ignored.
+
+-- *** Unary operators
+-- **** Function calls
+-- | When an expression evaluates to a function, you can call it using a list
+-- of arguments between parentheses. Most of the time, the function itself just
+-- comes from a scope variable, so a function call typically looks like this:
+
+-- | > {{ print(x, "hello!") }}
+
+-- | However, anything that returns a function can be called as a function:
+
+-- | > {{ system.console['log']("hello!") }}
+
+-- **** Filter expressions
+-- | Any function that takes at least one argument can be called through the
+-- alternative filter syntax instead:
+
+-- | > {{ x|print }}
+
+-- | This is equivalent to:
+
+-- | > {{ print(x) }}
+
+-- | Filters can take arguments:
+
+-- | > {{ x|append("foobar") }}
+
+-- | This is equivalent to:
+
+-- | > {{ append(x, "foobar") }}
+
+-- | /Deviation from Jinja2:/ Ginger does not distinguish between filters and
+-- functions at the semantics level; any function can be called as a filter,
+-- and vv., and the filter syntax is merely a syntactic variant of a function
+-- call.
 
 -- * Haskell API
 -- ** General
@@ -105,7 +220,7 @@ module Text.Ginger
 
 -- | An example call (for running a template in 'IO') would look something like
 -- this:
---
+
 -- > runGingerT (makeContextM scopeLookup (putStr . Text.unpack . htmlSource)) tpl
 , module Text.Ginger.Run
 -- ** Other concerns
