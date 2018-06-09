f5d7fd01760f923943c86fea7dd9e45c1262a67a
Tobias Dammers <tdammers@gmail.com>
2018-06-09 16:52:26 +0200

    Converted constraint inference to monadic style

--- src/Text/Ginger/Optimizer/ConstraintAnalyzer.hs
+++ src/Text/Ginger/Optimizer/ConstraintAnalyzer.hs
@@ -64,6 +64,10 @@ data Constraint
   | KnownValue (GVal Identity) -- ^ We actually know the value at compile time.
   deriving (Show)
 
+-- We need this stupid Ord instance because 'GVal' doesn't have a sensible one.
+-- It's not an entirely honest instance, really, but for the sake of putting
+-- it in a 'Map', it's good enough.
+
 instance Ord Constraint where
   compare = compareConstraint
 
@@ -144,6 +148,10 @@ addConstraint :: Constraint -> ScopeRef -> Evidence -> Evidence
 addConstraint constraint =
   alterConstraints (Set.insert constraint)
 
+addConstraints :: Constraints -> ScopeRef -> Evidence -> Evidence
+addConstraints constraints =
+  alterConstraints (<> constraints)
+
 removeConstraint :: Constraint -> ScopeRef -> Evidence -> Evidence
 removeConstraint constraint =
   alterConstraints (Set.delete constraint)
@@ -182,74 +190,89 @@ portEvidence :: ScopeRef -> Evidence -> Evidence
 portEvidence parentRef ev =
   Evidence . Map.mapKeys (parentRef <>) . unEvidence $ ev
 
+-- * The 'Infer' Monad
+
+type Infer = State Evidence
+
+runInfer :: Infer a -> Evidence -> (a, Evidence)
+runInfer action ev = runState action ev
+
+importEvidence :: ScopeRef -> Evidence -> Infer ()
+importEvidence parentRef ev =
+  modify (<> portEvidence parentRef ev)
+
+testify :: ScopeRef -> Constraints -> Infer ()
+testify ref constr =
+  modify (addConstraints constr ref)
+
+testifyHere :: Constraints -> Infer ()
+testifyHere = testify CurrentItem
+
 -- * Inferring Evidence
 
--- | Infer constraints for a given expression. In the returned pair, the
--- first element tells us the external constraints for the expression, while
--- the second element tells us how the expression affects the containing
--- scope. The reason we have to return them separately is because we need the
--- external constraints for the current expression when analyzing the
--- containing scope, but the expression itself doesn't know what it will be
--- bound to, so we have nowhere in the returned 'Evidence' to put it.
-inferExprConstraints :: Evidence -> Expression a -> (Evidence, Evidence)
+-- | Infer constraints for a given expression.
+inferExprConstraints :: Expression a -> Infer Evidence
 
 -- Literals are easy: they are always 'Static' and 'Constant', and they do not
 -- modify the environment.
-inferExprConstraints ev (StringLiteralE _ s) =
-  (singletonEvidence [Static, Constant, KnownValue (toGVal s)], ev)
-inferExprConstraints ev (NumberLiteralE _ n) =
-  (singletonEvidence [Static, Constant, KnownValue (toGVal n)], ev)
-inferExprConstraints ev (BoolLiteralE _ b) =
-  (singletonEvidence [Static, Constant, KnownValue (toGVal b)], ev)
-inferExprConstraints ev (NullLiteralE _) =
-  (singletonEvidence [Static, Constant, KnownValue def], ev)
+inferExprConstraints (StringLiteralE _ s) =
+  pure . singletonEvidence $ [Static, Constant, KnownValue (toGVal s)]
+inferExprConstraints (NumberLiteralE _ n) =
+  pure . singletonEvidence $ [Static, Constant, KnownValue (toGVal n)]
+inferExprConstraints (BoolLiteralE _ b) =
+  pure . singletonEvidence $ [Static, Constant, KnownValue (toGVal b)]
+inferExprConstraints (NullLiteralE _) =
+  pure . singletonEvidence $ [Static, Constant, KnownValue def]
 
 -- Variable lookups inherit the constraints of the thing they look up.
-inferExprConstraints ev (VarE _ varName) =
-  let selfEv = singletonEvidence $ getConstraints (NamedItem varName CurrentItem) ev
-  in (selfEv, ev)
+inferExprConstraints (VarE _ varName) =
+  singletonEvidence <$> gets (getConstraints (NamedItem varName CurrentItem))
 
 -- List literals give us two kinds of evidence. The first one is evidence about
 -- individual list items, which we simply inherit from the items themselves.
 -- The second one is that the list itself inherits all the constraints that
 -- hold for all of its members.
-inferExprConstraints ev (ListE _ xs) =
-  let (childEvidence, ev') =
-        inferChildEvidence ev
-          (zip [NumberedItem i CurrentItem | i <- [0..]] xs)
-      selfEvidence =
+inferExprConstraints (ListE _ xs) = do
+  childEvidence <- inferChildEvidence
+                      (zip [NumberedItem i CurrentItem | i <- [0..]] xs)
+  let selfEvidence =
         singletonEvidence .
           Map.foldl' Set.intersection [Static, Constant] .
           unEvidence $
           childEvidence
-  in (selfEvidence <> childEvidence, ev')
+  pure $ selfEvidence <> childEvidence
 
 -- Object literals are bit more complicated, because the keys may or may not
 -- be known at compile time. Worse yet, keys later in the definition overwrite
 -- earlier ones, so in { "foo": "bar", getKey(): "pizza" }, we don't really
 -- know anything about the "foo" key after all.
-inferExprConstraints ev (ObjectE _ xs) =
-  let (childEvidence, ev') =
-        inferChildPairEvidence ev xs
-      selfEvidence =
+inferExprConstraints (ObjectE _ xs) = do
+  childEvidence <- inferChildPairEvidence xs
+  let selfEvidence =
         singletonEvidence .
           Map.foldl' Set.intersection [Static, Constant] .
           unEvidence $
           childEvidence
-  in (selfEvidence <> childEvidence, ev')
+  pure $ selfEvidence <> childEvidence
+
+-- MemberLookupE a (Expression a) (Expression a) -- ^ foo[bar] (also dot access)
+-- CallE a (Expression a) [(Maybe Text, (Expression a))] -- ^ foo(bar=baz, quux)
+-- LambdaE a [Text] (Expression a) -- ^ (foo, bar) -> expr
+-- TernaryE a (Expression a) (Expression a) (Expression a) -- ^ expr ? expr : expr
+-- DoE a (Statement a) -- ^ do { statement; }
 
 -- By default, we assume that anything goes: any expression can in principle
 -- void any and all assumptions we can make about the current scope.
-inferExprConstraints ev expr =
-  (noEvidence, noEvidence)
+inferExprConstraints expr =
+  pure noEvidence
 
-inferChildEvidence :: Evidence -> [(ScopeRef, Expression a)] -> (Evidence, Evidence)
-inferChildEvidence ev ((ref, expr):exprs) =
-  let (myEvidence, ev') = inferExprConstraints ev expr
-      (tailEvidence, ev'') = inferChildEvidence ev' exprs
-  in (portEvidence ref myEvidence <> tailEvidence, ev'')
-inferChildEvidence ev [] =
-  (noEvidence, ev)
+inferChildEvidence :: [(ScopeRef, Expression a)] -> Infer Evidence
+inferChildEvidence ((ref, expr):exprs) = do
+  myEvidence <- inferExprConstraints expr
+  tailEvidence <- inferChildEvidence exprs
+  pure $ portEvidence ref myEvidence <> tailEvidence
+inferChildEvidence [] =
+  pure noEvidence
 
 -- | Given a set of known constraints on an expression, try to figure out the
 -- runtime expression value and turn it into a relative 'ScopeRef'. If this is
@@ -263,14 +286,14 @@ refFromKeyConstraints constraints =
     go (KnownValue gval:_) = Just $ NamedItem (asText gval) CurrentItem
     go (_:xs) = go xs
   
-inferChildPairEvidence :: Evidence -> [(Expression a, Expression a)] -> (Evidence, Evidence)
-inferChildPairEvidence ev ((keyExpr, valExpr):exprs) =
-  let (keyEvidence, ev') = inferExprConstraints ev keyExpr
-      (valEvidence, ev'') = inferExprConstraints ev' valExpr
-      (tailEvidence, ev''') = inferChildPairEvidence ev'' exprs
-      -- We can only generate useful relative evidence if we know the key at
-      -- compile time.
-      myEvidence =
+inferChildPairEvidence :: [(Expression a, Expression a)] -> Infer Evidence
+inferChildPairEvidence ((keyExpr, valExpr):exprs) = do
+  keyEvidence <- inferExprConstraints keyExpr
+  valEvidence <- inferExprConstraints valExpr
+  tailEvidence <- inferChildPairEvidence exprs
+  -- We can only generate useful relative evidence if we know the key at
+  -- compile time.
+  let myEvidence =
         case refFromKeyConstraints (getConstraints CurrentItem keyEvidence) of
           Nothing ->
             -- We don't know the key yet, so we cannot generate any evidence.
@@ -278,12 +301,12 @@ inferChildPairEvidence ev ((keyExpr, valExpr):exprs) =
           Just ref ->
             -- We do know the key, so we generate evidence for it.
             portEvidence ref valEvidence
-  in (myEvidence <> tailEvidence, ev''')
-inferChildPairEvidence ev [] =
-  (noEvidence, ev)
+  pure $ myEvidence <> tailEvidence
+inferChildPairEvidence [] =
+  pure noEvidence
 
-inferStmtConstraints :: Evidence -> Statement a -> Evidence
-inferStmtConstraints ev stmt = noEvidence
+inferStmtConstraints :: Statement a -> Infer Evidence
+inferStmtConstraints stmt = pure noEvidence
 
-inferTemplateConstraints :: Evidence -> Template a -> Evidence
-inferTemplateConstraints ev tpl = noEvidence
+inferTemplateConstraints :: Template a -> Infer Evidence
+inferTemplateConstraints tpl = pure noEvidence
