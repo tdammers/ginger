ad1a84c22a4343a1106c6032cee19ba11aab8d18
Tobias Dammers <tdammers@gmail.com>
2019-02-11 16:47:08 +0100

    Proper argument parser for the CLI

--- cli/GingerCLI.hs
+++ cli/GingerCLI.hs
@@ -19,6 +19,7 @@ import System.Environment ( getArgs )
 import System.IO
 import System.IO.Error
 import qualified Data.ByteString as BS
+import qualified Data.ByteString.UTF8 as UTF8
 import qualified Data.ByteString.Lazy as LBS
 import Control.Monad.Trans.Class ( lift )
 import Control.Monad.Trans.Maybe
@@ -26,6 +27,84 @@ import Control.Monad
 import Data.Default ( def )
 import System.Process as Process
 import Text.Printf (printf)
+import Options (parseOptions, Options (..), TemplateSource (..), DataSource (..))
+import System.Exit
+
+main :: IO ()
+main = do
+    args <- getArgs
+    options <- parseOptions args
+    case options of
+      RunOptions tpl dat ->
+        run tpl dat
+
+loadData :: DataSource -> IO (Maybe (HashMap Text JSON.Value))
+loadData (DataFromFile fn) = decodeFile fn
+loadData DataFromStdin = decodeStdin
+loadData (DataLiteral str) = decodeString str
+
+loadTemplate :: TemplateSource -> IO (Template SourcePos)
+loadTemplate tplSrc = do
+    let resolve = loadFileMay
+    (tpl, src) <- case tplSrc of
+        TemplateFromFile fn -> (,) <$> parseGingerFile resolve fn <*> return Nothing
+        TemplateFromStdin -> getContents >>= \s -> (,) <$> parseGinger resolve Nothing s <*> return (Just s)
+
+    case tpl of
+        Left err -> do
+            tplSource <-
+                case src of
+                    Just s ->
+                        return (Just s)
+                    Nothing -> do
+                        let s = sourceName <$> peSourcePosition err
+                        case s of
+                            Nothing -> return Nothing
+                            Just sn -> Just <$> loadFile sn
+            printParserError tplSource err
+            exitFailure
+        Right t -> do
+            return t
+
+
+run :: TemplateSource -> DataSource -> IO ()
+run tplSrc dataSrc = do
+    scope <- loadData dataSrc
+    let scopeLookup key = toGVal (scope >>= HashMap.lookup key)
+    let contextLookup :: Text -> Run p IO Html (GVal (Run p IO Html))
+        contextLookup key =
+            case key of
+                "print" -> return printF
+                "system" -> return systemF
+                _ -> return $ scopeLookup key
+    let context =
+            makeContextHtmlExM
+                contextLookup
+                (putStr . Text.unpack . htmlSource)
+                (hPutStrLn stderr . show)
+
+    tpl <- loadTemplate tplSrc
+    runGingerT context tpl >>= either (hPutStrLn stderr . show) showOutput
+    where
+      showOutput value
+        | isNull value = return ()
+        | otherwise = putStrLn . show $ value
+
+
+printParserError :: Maybe String -> ParserError -> IO ()
+printParserError srcMay = putStrLn . formatParserError srcMay
+
+displayParserError :: String -> ParserError -> IO ()
+displayParserError src pe = do
+    case peSourcePosition pe of
+        Just pos -> do
+            let ln = Prelude.take 1 . Prelude.drop (sourceLine pos - 1) . Prelude.lines $ src
+            case ln of
+                [] -> return ()
+                x:_ -> do
+                    putStrLn x
+                    putStrLn $ Prelude.replicate (sourceColumn pos - 1) ' ' ++ "^"
+        _ -> return ()
 
 loadFile fn = openFile fn ReadMode >>= hGetContents
 
@@ -39,6 +118,12 @@ loadFileMay fn =
 
 decodeFile :: (JSON.FromJSON v) => FilePath -> IO (Maybe v)
 decodeFile fn = YAML.decode <$> (openFile fn ReadMode >>= BS.hGetContents)
+
+decodeString :: (JSON.FromJSON v) => String -> IO (Maybe v)
+decodeString = return . YAML.decode . UTF8.fromString
+
+decodeStdin :: (JSON.FromJSON v) => IO (Maybe v)
+decodeStdin = YAML.decode <$> BS.getContents
 
 printF :: GVal (Run p IO Html)
 printF = fromFunction $ go
@@ -66,69 +151,3 @@ gAsStr = Text.unpack . asText
 strToGVal :: String -> GVal m
 strToGVal = toGVal . Text.pack
 
-main = do
-    args <- getArgs
-    let (srcFn, scopeFn) = case args of
-            [] -> (Nothing, Nothing)
-            a:[] -> (Just a, Nothing)
-            a:b:[] -> (Just a, Just b)
-
-    scope <- case scopeFn of
-        Nothing -> return Nothing
-        Just fn -> (decodeFile fn :: IO (Maybe (HashMap Text JSON.Value)))
-
-    let scopeLookup key = toGVal (scope >>= HashMap.lookup key)
-        resolve = loadFileMay
-    let contextLookup :: Text -> Run p IO Html (GVal (Run p IO Html))
-        contextLookup key =
-            case key of
-                "print" -> return printF
-                "system" -> return systemF
-                _ -> return $ scopeLookup key
-
-    (tpl, src) <- case srcFn of
-            Just fn -> (,) <$> parseGingerFile resolve fn <*> return Nothing
-            Nothing -> getContents >>= \s -> (,) <$> parseGinger resolve Nothing s <*> return (Just s)
-
-    -- TODO: do some sort of arg parsing thing so that we can turn
-    -- template dumping on or off.
-    -- print tpl
-
-    case tpl of
-        Left err -> do
-            tplSource <-
-                case src of
-                    Just s ->
-                        return (Just s)
-                    Nothing -> do
-                        let s = sourceName <$> peSourcePosition err
-                        case s of
-                            Nothing -> return Nothing
-                            Just sn -> Just <$> loadFile sn
-            printParserError tplSource err
-        Right t -> do
-            let context =
-                    makeContextHtmlExM
-                        contextLookup
-                        (putStr . Text.unpack . htmlSource)
-                        (hPutStrLn stderr . show)
-            runGingerT context t >>= either (hPutStrLn stderr . show) showOutput
-    where
-      showOutput value
-        | isNull value = return ()
-        | otherwise = putStrLn . show $ value
-
-printParserError :: Maybe String -> ParserError -> IO ()
-printParserError srcMay = putStrLn . formatParserError srcMay
-
-displayParserError :: String -> ParserError -> IO ()
-displayParserError src pe = do
-    case peSourcePosition pe of
-        Just pos -> do
-            let ln = Prelude.take 1 . Prelude.drop (sourceLine pos - 1) . Prelude.lines $ src
-            case ln of
-                [] -> return ()
-                x:_ -> do
-                    putStrLn x
-                    putStrLn $ Prelude.replicate (sourceColumn pos - 1) ' ' ++ "^"
-        _ -> return ()
---
+++ cli/Options.hs
@@ -0,0 +1,69 @@
+module Options
+where
+
+import Options.Applicative
+
+
+data TemplateSource
+  = TemplateFromFile FilePath
+  | TemplateFromStdin
+
+data DataSource
+  = DataFromFile FilePath
+  | DataLiteral String
+  | DataFromStdin
+
+data Options
+  = RunOptions TemplateSource DataSource
+
+parseOptions :: [String] -> IO Options
+parseOptions args =
+  execParser $ info (options <**> helper)
+    ( fullDesc
+    <> header "ginger - A command-line interface for the Ginger template language"
+    )
+
+options :: Parser Options
+options = runOptions
+
+runOptions :: Parser Options
+runOptions =
+  RunOptions <$> templateSource <*> dataSource
+
+templateSource :: Parser TemplateSource
+templateSource =
+  convert <$> option auto
+      ( long "template"
+      <> short 't'
+      <> metavar "TEMPLATE"
+      <> help "Load ginger template from this file"
+      <> value "-"
+      )
+  where
+    convert "-" = TemplateFromStdin
+    convert f = TemplateFromFile f
+
+dataSource :: Parser DataSource
+dataSource =
+  dataFromFile <|> dataLiteral
+
+dataFromFile :: Parser DataSource
+dataFromFile =
+  convert <$> option auto
+        ( long "data-file"
+        <> metavar "DATAFILE"
+        <> help "Load JSON or YAML data from this file (`-' to read from stdin)"
+        )
+  where
+    convert "-" = DataFromStdin
+    convert f = DataFromFile f
+
+dataLiteral :: Parser DataSource
+dataLiteral =
+  DataLiteral <$> option auto
+        ( long "data"
+        <> short 'd'
+        <> metavar "DATA"
+        <> help "Use specified (JSON or YAML) DATA"
+        <> value "{}"
+        )
