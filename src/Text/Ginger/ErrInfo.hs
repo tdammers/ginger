920b7879a3fd824ee2c2024c83cc6d06dd40acb5
Eric Conlon <ejconlon@gmail.com>
2020-05-13 10:39:28 -0700

    fix comment style and bump lts

--- src/Text/Ginger/ErrInfo.hs
+++ src/Text/Ginger/ErrInfo.hs
@@ -4,7 +4,7 @@ module Text.Ginger.ErrInfo
     ( ErrInfo (..)
     ) where
 
-{-| A newtype around 'Either' that lets us define a useful 'MonadFail' instance. -}
+-- | A newtype around 'Either' that lets us define a useful 'MonadFail' instance.
 newtype ErrInfo a = ErrInfo { runErrInfo :: Either String a } deriving (Functor, Applicative, Monad)
 
 instance MonadFail ErrInfo where
